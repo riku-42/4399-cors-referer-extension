e15bfa9472c9d46a26adbca73113d66ab16e613d
riku_42 <otogurohana@gmail.com>
2024-08-09 22:23:28 +0800

    Set referer and cors rules

--- src/background/index.js
+++ src/background/index.js
@@ -5,3 +5,58 @@ chrome.runtime.onMessage.addListener((request) => {
     console.log('background has received a message from popup, and count is ', request?.count)
   }
 })
+
+chrome.runtime.onInstalled.addListener(() => {
+  const rules = [
+    {
+      id: 1,
+      priority: 1,
+      condition: {
+        urlFilter: '*://*/*',
+        resourceTypes: [
+          'main_frame',
+          'sub_frame',
+          'xmlhttprequest',
+          'script',
+          'stylesheet',
+          'font',
+        ],
+      },
+      action: {
+        type: 'modifyHeaders',
+        responseHeaders: [
+          {
+            header: 'Access-Control-Allow-Origin',
+            operation: 'set',
+            value: '*',
+          },
+        ],
+        requestHeaders: [
+          {
+            header: 'Referer',
+            operation: 'set',
+            value: 'https://www.4399.com',
+          },
+        ],
+      },
+    },
+  ]
+
+  chrome.declarativeNetRequest.updateDynamicRules(
+    {
+      removeRuleIds: rules.map((rule) => rule.id),
+      addRules: rules,
+    },
+    () => {
+      if (chrome.runtime.lastError) {
+        console.error('Error adding rules: ' + chrome.runtime.lastError)
+      } else {
+        console.log('Rules added successfully')
+      }
+    },
+  )
+})
+
+chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((details) => {
+  console.log('onRuleMatchedDebug', details)
+})
